f16ed85841a42f9c542789c83aac46cd5fc22133
Nikita <n.karpinskiy@smartdesign.by>
2018-04-20 15:35:35 +0300

    Add block work and pictures

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,26 @@
+
+$(document).ready(function() {
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        // autoplay:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+
+});
+
 //
 // /* Этот код будет работать по современному стандарту ES5
 //  ========================= */
